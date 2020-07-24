ae57357793d8b875d3860d557ff79ca6563a92c7
makarand <makarandbhale123@gmail.com>
2020-07-24 13:36:22 +0530

    fourth commit

---
+++ credentials/mongo_db_creds.py
@@ -0,0 +1,5 @@
+LOCAL_MONGO_URI = "mongodb://127.0.0.1:27017"
+LOCAL_MONGO_DB_URI = "mongodb://127.0.0.1:27017/satori"
+
+MLAB_MONGO_DB_URI = "mongodb+srv://satori-root:b#Sha_12@cluster0.38tvr.mongodb.net/satori?retryWrites=true&w=majority"
+MLAB_MONGO_URI = "mongodb+srv://satori-root:b#Sha_12@cluster0.38tvr.mongodb.net/"
---
+++ credentials/redis_creds.py
@@ -0,0 +1,1 @@
+REMOTE_HOST = 'http://satori-env.eba-d5hujvqd.ap-south-1.elasticbeanstalk.com/'
